c9d8a66d177df15f6b08625dbc6c0a5089d1b2c3
khanh <tranvietkhanh2k5hp@gmail.com>
2025-07-07 00:20:55 +0700

    sửa tí phần chi tiết order

--- HPsneaker/app/Models/Product.php
+++ HPsneaker/app/Models/Product.php
@@ -12,7 +12,7 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     public function variants()
