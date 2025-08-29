c4330cc94c2b9d43d6ca7c6973e6875bfc45f7e0
sang2502 <sangbcpp03045@fpt.edu.vn>
2025-08-29 22:44:45 +0700

    sửa voucher v2

--- HPsneaker/resources/views/admin/pos/update.blade.php
+++ HPsneaker/resources/views/admin/pos/update.blade.php
@@ -156,8 +156,6 @@
         </div>
     </div>
 </div>
-
-<!-- Thêm jQuery nếu chưa có -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     // Lọc sản phẩm theo tên
