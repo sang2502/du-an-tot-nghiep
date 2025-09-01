4128c6986551d638d7fe75a1f89cbdc1bf099933
Hieu <heudao20456@gmail.com>
2025-09-01 10:24:42 +0700

    Fix lỗi xoá mềm

--- HPsneaker/database/migrations/2025_07_23_134718_create_reviews_table.php
+++ HPsneaker/database/migrations/2025_07_23_134718_create_reviews_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('rating');
             $table->text('cmt')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
